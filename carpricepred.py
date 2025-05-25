1db1844b401284e8f1152784358631f223297959
elifsenasen <74709961+elifsenasen@users.noreply.github.com>
2025-05-26 00:54:47 +0300

    Add files via upload
    Final changes

---
+++ carpricepred.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import xgboost as xgb
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+import lightgbm as lgb
+from pydantic import BaseModel
+
+
+def dataframe():
+    df= pd.read_csv("car data.csv")
+    missing_data = df.isnull().sum()
+    if missing_data.sum() > 0:
+        df = df.fillna(df.median())
+    df['age'] = 2025 - df['Year']
+    df.drop('Year',axis=1,inplace = True)
+    return df
+
+def remove_outlier(df):
+    for col in ["Kms_Driven", "age", "Selling_Price"]:
+        first_qntl=df[col].quantile(0.25)
+        third_qntl=df[col].quantile(0.75)
+        iqr=third_qntl-first_qntl
+        lower_limit = first_qntl - 1.5 * iqr
+        upper_limit=third_qntl+1.5*iqr
+        df[col] = df[col].clip(lower=lower_limit, upper=df[col].quantile(0.90))
+    return df
+
+def final_df():
+    df = dataframe()
+    new_df = remove_outlier(df)
+    return new_df
+
+df=final_df()
+scaler = MinMaxScaler(feature_range=(0, 1))
+df[['Kms_Driven','age',"Present_Price"]] = scaler.fit_transform(df[['Kms_Driven','age',"Present_Price"]])
+mean = df[['Kms_Driven', 'age', 'Present_Price']].mean()
+std = df[['Kms_Driven', 'age', 'Present_Price']].std()
+
+encoder = OneHotEncoder(sparse_output=False, drop='first')
+categorical_cols = ['Fuel_Type', 'Seller_Type', 'Transmission']
+encoded_cols = encoder.fit_transform(df[categorical_cols])
+encoded_feature_names = encoder.get_feature_names_out(categorical_cols)
+encoded_df = pd.DataFrame(encoded_cols, columns=encoded_feature_names)
+df = pd.concat([df.drop(columns=categorical_cols), encoded_df], axis=1)
+
+# set split
+x=df.drop(columns=["Selling_Price","Car_Name"])
+y=df["Selling_Price"]
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+
+def random_forest():
+    rf = RandomForestRegressor(
+        n_estimators=500,
+        max_depth=10,
+        min_samples_split=4,
+        min_samples_leaf=2,
+        max_features='sqrt',
+        random_state=42
+    )
+    rf.fit(x_train, y_train)
+    return rf
+
+def linearRegression():
+    lr=LinearRegression()
+    lr.fit(x_train,y_train)
+    return lr
+
+def xgBoost():
+    xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1)
+    xgb_model.fit(x_train,y_train)
+    return xgb_model
+
+def svr():
+    svr = SVR(kernel='rbf', C=100, epsilon=0.1)
+    svr.fit(x_train, y_train)
+    return svr
+
+def LGB():
+    lgb_model=lgb.LGBMRegressor(n_estimators=100, learning_rate=0.1, max_depth=-1,min_split_gain=0.1)
+    lgb_model.fit(x_train,y_train)
+    return lgb_model
+
+def evulatemodel(model):
+    y_train_pred = model.predict(x_train)
+    mse = mean_squared_error(y_train, y_train_pred)
+    mae = mean_absolute_error(y_train, y_train_pred)
+    r2 = r2_score(y_train, y_train_pred)
+
+    y_test_pred = model.predict(x_test)
+    mse_test = mean_squared_error(y_test, y_test_pred)
+    mae_test = mean_absolute_error(y_test, y_test_pred)
+    r2_test = r2_score(y_test, y_test_pred)
+    
+
+    return {
+        r2_test
+    }
+
+
+class Car(BaseModel):
+    Year: int
+    Present_price: float
+    Kms_driven: int
+    Fuel_Type: str
+    Seller_Type: str
+    Transmission: str
+    Owner: int
+
+
+
+def newinput(model, car: Car):
+    new_data = {
+        'Year': car.Year,
+        'Present_Price': car.Present_price,
+        'Kms_Driven': car.Kms_driven,
+        'Fuel_Type': car.Fuel_Type,
+        'Seller_Type': car.Seller_Type,
+        'Transmission': car.Transmission,
+        'Owner': car.Owner
+    }
+
+    new_df = pd.DataFrame([new_data])
+    new_df['age'] = 2025 - new_df['Year']
+    new_df.drop('Year', axis=1, inplace=True)
+
+    encoded_cols = encoder.transform(new_df[categorical_cols])
+    encoded_df = pd.DataFrame(encoded_cols, columns=encoder.get_feature_names_out(categorical_cols))
+
+    new_df = pd.concat([new_df.drop(columns=categorical_cols), encoded_df], axis=1)
+    new_df = new_df.reindex(columns=x_train.columns, fill_value=0)
+    new_df[['Kms_Driven', 'age', 'Present_Price']] = scaler.transform(new_df[['Kms_Driven', 'age', 'Present_Price']])
+
+    prediction = model.predict(new_df)
+    return prediction[0]
+
+
+
---
+++ main.dart
@@ -0,0 +1,412 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MaterialApp(home: CarPriceApp()));
+
+class CarPriceApp extends StatefulWidget {
+  const CarPriceApp({super.key});
+
+  @override
+  _CarPriceAppState createState() => _CarPriceAppState();
+}
+
+class _CarPriceAppState extends State<CarPriceApp> {
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController kmsController = TextEditingController();
+  final TextEditingController yearController= TextEditingController();
+
+  String? selectedFuelType;
+  String? selectedSellerType;
+  String? selectedTransmission;
+  String? selectedOwner;
+  String? selectedModel;
+  String? selectedPlot;
+  String currentGif = "";
+  Key gifKey = UniqueKey();
+
+  bool _isLoading = false; 
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  @override
+    void initState() {
+  super.initState();
+  currentGif = "assets/gifs/initial.gif"; 
+}
+
+  void _updateGif(String path) {
+    setState(() {
+      currentGif = path;
+      gifKey = UniqueKey();
+    });
+  }
+
+  Widget buildInput({
+  required String label,
+  required String? selectedValue,
+  required List<String> items,
+  required Function(String?) onChanged,
+  required String gifPath,
+}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: Container(
+      width: double.infinity, 
+      child: DropdownButtonFormField<String>(
+        value: selectedValue,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(), 
+        ),
+        onTap: () => _updateGif(gifPath),
+        items: items.map((String value) {
+          return DropdownMenuItem<String>(value: value, child: Text(value));
+        }).toList(),
+      ),
+    ),
+  );
+}
+  void showPredictionOverlay(BuildContext context, double predictedPrice, double accuracy, double? realPrice) {
+    if (realPrice==null)return;
+
+    double minValue = 0;
+    double maxValue = realPrice; 
+    double percentage = (predictedPrice / realPrice).clamp(0.0, 1.0);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          title: Text("Predicted Price", textAlign: TextAlign.center),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "\$${predictedPrice.toStringAsFixed(0)}K",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Accuracy: ${accuracy.toStringAsFixed(2)}",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+              SizedBox(height: 20),
+              LinearProgressIndicator(
+                value: percentage.clamp(0.0, 1.0),
+                minHeight: 20,
+                backgroundColor: Colors.grey[300],
+                color: Colors.green,
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("\$${minValue.toInt()}"),
+                  Text("\$${maxValue.toInt()}K"),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Exit"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+void _showErrorDialog(String message) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text("Error"),
+      content: Text(message),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text("OK"),
+        ),
+      ],
+    ),
+  );
+}
+
+Future<void> _showPlot(String plotName) async {
+  try {
+    final String plotUrl = "http://localhost:5266/CarPrice/plot/$plotName";
+    final response = await http.get(Uri.parse(plotUrl));
+
+    if (response.statusCode == 200) {
+      final imageBytes = response.bodyBytes;
+      showDialog(
+        context: context,
+        builder: (context) => Dialog(child: Image.memory(imageBytes)),
+      );
+    } else {
+      _showErrorDialog("An error occurred. Please try again later.");
+    }
+  } catch (e) {
+    _showErrorDialog("An error occurred. Please try again later.");
+  }
+}
+
+
+  Future<void> _predictPrice() async {
+    if (selectedModel == null) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text("Warning"),
+      content: Text("Please select a model before predicting."),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text("OK"),
+        ),
+      ],
+    ),
+  );
+  return;
+  }
+    
+    final String apiUrl = "http://localhost:5266/CarPrice/predict/$selectedModel";
+    final String evaluateUrl= "http://localhost:5266/CarPrice/evaluate/$selectedModel";
+
+    final carData = {
+      'Year': int.tryParse(yearController.text),
+      'Present_price': double.tryParse(priceController.text),
+      'Kms_driven': int.tryParse(kmsController.text),
+      'Fuel_Type': selectedFuelType,
+      'Seller_Type': selectedSellerType,
+      'Transmission': selectedTransmission,
+      'Owner': selectedOwner != null ? int.parse(selectedOwner!) : null,
+    };
+    double? realPrice = double.tryParse(priceController.text);
+    print("Car data being sent: ${jsonEncode(carData)}");
+
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(carData),
+      );
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+
+      if (responseData.containsKey('predicted_Price')) {
+        final double predictedPrice = responseData['predicted_Price'] as double;
+        final evalResponse = await http.post(Uri.parse(evaluateUrl));
+        final evalData = jsonDecode(evalResponse.body);
+        final double accuracy = evalData['results'];
+
+        await _audioPlayer.play(
+          AssetSource('sounds/success.mp3'),
+        ); 
+
+        showPredictionOverlay(context, predictedPrice, accuracy, realPrice);
+      } else {
+  
+        showDialog(
+          context: context,
+          builder:
+              (context) => AlertDialog(
+                title: Text("Error"),
+                content: Text(
+                  "Could not predict the car price. Please try again.",
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text("Ok"),
+                  ),
+                ],
+              ),
+        );
+      }
+    } catch (e) {
+     _showErrorDialog("An error occurred. Please try again later.");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Car Price Predictor")),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (currentGif.isNotEmpty)
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      currentGif,
+                      key: gifKey,
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              SizedBox(height: 15),
+              buildInput(
+                label: "Selected Model",
+                selectedValue: selectedModel,
+                items: ['lr', 'rf', 'xgb', 'svr', 'lgb'],
+                onChanged:
+                    (value) => setState(() {
+                      selectedModel = value;
+                    }),
+                gifPath: "assets/gifs/model.gif",
+              ),
+              buildInput(
+                label: "Fuel Type",
+                selectedValue: selectedFuelType,
+                items: ["Petrol", "Diesel"],
+                onChanged:
+                    (value) => setState(() {
+                      selectedFuelType = value;
+                    }),
+                gifPath: "assets/gifs/fuel.gif",
+              ),
+              buildInput(
+                label: "Seller Type",
+                selectedValue: selectedSellerType,
+                items: ["Dealer", "Individual"],
+                onChanged:
+                    (value) => setState(() {
+                      selectedSellerType = value;
+                    }),
+                gifPath: "assets/gifs/seller.gif",
+              ),
+              buildInput(
+                label: "Transmission Type",
+                selectedValue: selectedTransmission,
+                items: ["Manual", "Automatic"],
+                onChanged:
+                    (value) => setState(() {
+                      selectedTransmission = value;
+                    }),
+                gifPath: "assets/gifs/transmission.gif",
+              ),
+              buildInput(
+                label: "Number of Owners",
+                selectedValue: selectedOwner,
+                items: ["0","1","2","3","4","5","6","7","8"],
+                onChanged:
+                    (value) => setState(() {
+                      selectedOwner = value;
+                    }),
+                gifPath: "assets/gifs/owner.gif",
+              ),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  controller: yearController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Manufacturing Year",
+                    border: OutlineInputBorder(),
+                  ),
+                  onTap: () => _updateGif("assets/gifs/year.gif"),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  controller: priceController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: "Present Price (\$)",
+                    border: OutlineInputBorder(),
+                  ),
+                  onTap: () => _updateGif("assets/gifs/price.gif"),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  controller: kmsController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Kms Driven",
+                    border: OutlineInputBorder(),
+                  ),
+                  onTap: () => _updateGif("assets/gifs/kms.gif"),
+                ),
+              ),
+               buildInput(
+                label: "Plot Type(Optional)",
+                selectedValue: selectedPlot,
+                items: ["km", "fuel","age","outliers","removed_outliers"],
+                onChanged:
+                    (value) => setState(() {
+                      selectedPlot = value;
+                    }),
+                gifPath: "assets/gifs/plot.gif",
+              ),
+              SizedBox(height: 20),
+              if (_isLoading)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: CircularProgressIndicator(),
+              )
+            else
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: _predictPrice,
+                    child: Text("Predict"),
+                  ),
+                  SizedBox(height: 15),
+                ElevatedButton(
+                onPressed: selectedPlot == null
+                    ? null
+                    : () {
+                        _showPlot(selectedPlot!);
+                      },
+                child: Text("Show Plot"),
+              ),
+
+                ],
+              )
+
+                
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
